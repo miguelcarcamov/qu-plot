6f041a38417a82ff9b9e67c1b2525863c0b621b8
Miguel Carcamo <miguel.carcamo@protonmail.com>
2023-02-20 11:19:23 -0300

    CHG: adding read and write to catalogio

--- src/qu_plot/io/catalog_io.py
+++ src/qu_plot/io/catalog_io.py
@@ -1,12 +1,22 @@
 from dataclasses import dataclass
 
+import astropy.table as tbl
+from astropy.io import fits
+
 from .io import Io
 
 
 @dataclass
 class CatalogIo(Io):
     def read(self):
-        pass
+        with fits.open(self.input_name) as hdu_list:
+            catalog = tbl.Table.read(hdu_list, format="fits")
+        return catalog
 
-    def write(self):
-        pass
+    def write(
+        self,
+        catalog: tbl.Table = None,
+        overwrite: bool = True,
+        file_format: str = "fits",
+    ):
+        catalog.write(catalog, overwrite=overwrite, format=file_format)
